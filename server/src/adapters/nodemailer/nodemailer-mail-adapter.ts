b5a4ccb949c4c48b52f39fd2497c86e589e2170f
lara123-pg <lamorimalvescavalcante@gmail.com>
2022-05-06 13:17:13 -0300

    NLW Return - Server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "9e240851b7e73a",
+      pass: "df8269a3a1fe77"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Lara Fernanda <lamorimalvescavalcante@gmail.com>',
+            subject,
+            html: body
+        })
+    };
+}
